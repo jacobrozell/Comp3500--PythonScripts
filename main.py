3e537ac5c5938b0d7381df31877d239b48344977
Jacob Rozell <jacob.rozell83@gmail.com>
2019-06-09 22:54:50 -0500

    Cleaned/Roated x/y axis

--- main.py
+++ main.py
@@ -8,41 +8,50 @@ def create_files(fileIn, fileOut, csvFileOut):
                         or line.startswith("A"):
                     parsedFile.write(line)
 
-    unknowns = ""
-    pids = ""
-    jats = ""
-    tjds = ""
-    mems = ""
+    turnAroundTimes = []
+    pids = []
+    jats = []
+    tjds = []
+    mems = []
     comma = ","
 
-    with open(csvFileOut, 'w') as csv:
-        with open(fileOut) as file1:
-            for line in file1:
-                if line.startswith("*") or line.startswith("P") or line.startswith("N") or line.startswith("A"):
-                    continue
-                else:
-                    # make line look like PID, JAT, TJD
-                    unknownValue = line[1:12].replace(" ", "") + ","
-                    pidValue = line[19:22].replace(" ", "") + ","
-                    jatValue = line[29:39].replace(" ", "") + ","
-                    tjdValue = line[45:55].replace(" ", "") + ","
-                    memValue = line[63:67].replace(" ", "") + ","
-
-                    unknowns += unknownValue
-                    pids += pidValue
-                    jats += jatValue
-                    tjds += tjdValue
-                    mems += memValue
+    with open(fileOut) as file1:
+        for line in file1:
+            if line.startswith("*") or line.startswith("P") or line.startswith("N") or line.startswith("A"):
+                continue
 
             else:
-                csv.write(unknowns)
-                csv.write("\n")
-                csv.write(pids)
-                csv.write("\n")
-                csv.write(jats)
-                csv.write("\n")
-                csv.write(tjds)
-                csv.write("\n")
-                csv.write(mems)
-                csv.write("\n")
+                # make line look like PID, JAT, TJD
+                turnAround = line[1:12].replace(" ", "")
+                pidValue = line[19:22].replace(" ", "")
+                jatValue = line[29:39].replace(" ", "")
+                tjdValue = line[45:55].replace(" ", "")
+                memValue = line[63:67].replace(" ", "")
+
+                turnAroundTimes.append(turnAround)
+                pids.append(pidValue)
+                jats.append(jatValue)
+                tjds.append(tjdValue)
+                mems.append(memValue)
+
+    with open(csvFileOut, 'w') as csv:
+        count = -1
+        while count < 249:
+            count += 1
+            stringToWrite = str(pids[count] + comma + jats[count] + comma + \
+                             turnAroundTimes[count] + comma + tjds[count] + comma + mems[count] + "\n")
+
+            csv.write(stringToWrite)
+
+
+create_files("fcfs.txt", "fcfsParsed.txt", "fcfsCsv.txt")
+create_files("sjf.txt", "sjfParsed.txt", "sjfCsv.txt")
+
+create_files("rr1.txt", "rr1Parsed.txt", "rr1Csv.txt")
+create_files("rr5.txt", "rr5Parsed.txt", "rr5Csv.txt")
+create_files("rr10.txt", "rr10Parsed.txt", "rr10Csv.txt")
+create_files("rr15.txt", "rr15Parsed.txt", "rr15Csv.txt")
+create_files("rr20.txt", "rr20Parsed.txt", "rr20Csv.txt")
+create_files("rr25.txt", "rr25Parsed.txt", "rr25Csv.txt")
+create_files("rr50.txt", "rr50Parsed.txt", "rr50Csv.txt")
 
